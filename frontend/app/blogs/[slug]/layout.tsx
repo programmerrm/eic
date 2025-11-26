69ab402eda9d9499d855ec74560403a484d04e29
programmerrm <programmerwebrm@gmail.com>
2025-11-27 01:11:40 +0600

    frontend problem solving

--- frontend/app/blogs/[slug]/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function SingleBlogRootLayout({
-    children,
-}: {
-    children: React.ReactNode;
-}) {
-    return <main>{children}</main>;
-}
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,19 +1,7 @@
-import HomePageRelated from "@/components/services/homepageRelated";
-import Banner from "../components/banner/banner";
-import Cybersecurity from "../components/cybersecurity/cybersecurity";
-import Features from "../components/features/features";
-import OurProvenProcessSecurity from "../components/our-proven-process-security/our-proven-process-security";
-import SecurityFirm from "../components/security-firm/security-firm";
-
 export default function Page() {
     return (
         <>
-            <Banner />
-            <SecurityFirm />
-            <Features />
-            <Cybersecurity />
-            <HomePageRelated />
-            <OurProvenProcessSecurity />
+            
         </>
     );
 }
