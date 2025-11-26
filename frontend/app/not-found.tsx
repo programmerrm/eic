481c618844ced8f0cee0eb6a17302eca6cb7e183
programmerrm <programmerwebrm@gmail.com>
2025-11-27 00:08:36 +0600

    frontend problem solving

--- frontend/app/not-found.tsx
+++ frontend/app/not-found.tsx
@@ -1,33 +1,21 @@
 import Image from "next/image";
-import { MEDIA_URL } from "../utils/api";
-import { getFetchData } from "../utils/getFetchData";
 import NotFoundImg from "../public/images/not-found.svg";
 
 export default async function NotFound() {
-    const not_found_data = await getFetchData('/configuration/not-found-content/');
     return (
         <section className="error-section">
             <div className="container">
                 <div className="error-image">
-                    {not_found_data?.data?.image ? (
-                        <Image
-                            src={`${MEDIA_URL}${not_found_data?.data?.image}`}
-                            alt="not-found-icon"
-                            width={302}
-                            height={300}
-                        />
-                    ) : (
-                        <Image
-                            src={NotFoundImg}
-                            alt="not-found-icon"
-                            width={302}
-                            height={300}
-                        />
-                    )}
+                    <Image
+                        src={NotFoundImg}
+                        alt="not-found-icon"
+                        width={302}
+                        height={300}
+                    />
                 </div>
                 <div className="error-content">
-                    <h4>{not_found_data?.data?.title || "Oops! This page has gone missing."}</h4>
-                    <p>{not_found_data?.data?.description || "Uh-oh! This page doesn't exist. It might’ve moved or been deleted. Head to our homepage or explore below!"}</p>
+                    <h4>Oops! This page has gone missing.</h4>
+                    <p>Uh-oh! This page doesn exist. It might ve moved or been deleted. Head to our homepage or explore below!</p>
                 </div>
             </div>
         </section>
