5eed676f9592650572595486503d0dabc6a0cfb8
programmerrm <programmerwebrm@gmail.com>
2025-11-28 12:45:06 +0600

    new setup deploy.prod.sh

--- deploy.prod.sh
+++ deploy.prod.sh
@@ -21,24 +21,6 @@ echo "STEP FIVE ::: COLLECT STATIC FILES"
 docker-compose -f docker-compose.prod.yml exec backend python manage.py collectstatic --noinput
 
 echo "STEP SIX ::: CREATE DJANGO SUPERUSER IF NOT EXISTS"
-docker-compose -f docker-compose.prod.yml exec -T backend python manage.py shell -c "
-import os
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
-email = os.getenv('DJANGO_SUPERUSER_EMAIL', 'info@eic.com.bd')
-username = os.getenv('SUPERUSER_USERNAME', 'eicadmin')
-password = os.getenv('SUPERUSER_PASSWORD', 'strongpassword123')
-
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(email=email, username=username, password=password)
-    print('SUPERUSER CREATED SUCCESSFULLY!')
-else:
-    print('SUPERUSER ALREADY EXISTS.')
-"
-
-echo "STEP SEVEN ::: NGINX CONFIGURATION"
 docker-compose -f docker-compose.prod.yml exec nginx nginx -t
 
 echo "STEP EIGHT ::: RELOAD NGINX"
