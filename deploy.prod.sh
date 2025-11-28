3bb02b10808f4d20268c8a13f96a301fceba981c
programmerrm <programmerwebrm@gmail.com>
2025-11-28 11:55:41 +0600

    new setup deploy.prod.sh

--- deploy.prod.sh
+++ deploy.prod.sh
@@ -1,10 +1,12 @@
+#!/bin/bash
 set -e
 set -a
 source ./backend/.env.prod
 set +a
 
-echo "STEP ONE ::: UN-USED CLEAN UP DOCKER"
-docker system prune -f
+echo "STEP ONE ::: CLEAN UP UNUSED DOCKER RESOURCES"
+# Optional, সাবধানে ব্যবহার করো
+# docker system prune -f
 
 echo "STEP TWO ::: BUILD DOCKER IMAGES"
 docker-compose -f docker-compose.prod.yml build
@@ -13,7 +15,6 @@ echo "STEP THREE ::: START CONTAINERS"
 docker-compose -f docker-compose.prod.yml up -d
 
 echo "STEP FOUR ::: DATABASE MIGRATION"
-docker-compose -f docker-compose.prod.yml exec backend python manage.py makemigrations --noinput
 docker-compose -f docker-compose.prod.yml exec backend python manage.py migrate --noinput
 
 echo "STEP FIVE ::: COLLECT STATIC FILES"
@@ -28,7 +29,7 @@ User = get_user_model()
 
 email = os.getenv('DJANGO_SUPERUSER_EMAIL', 'info@eic.com.bd')
 username = os.getenv('SUPERUSER_USERNAME', 'eicadmin')
-password = os.getenv('SUPERUSER_PASSWORD', 'strongpassword123')
+password = os.getenv('SUPERUSER_PASSWORD', 'StrongPass123!')
 
 if not User.objects.filter(username=username).exists():
     User.objects.create_superuser(email=email, username=username, password=password)
@@ -37,10 +38,10 @@ else:
     print('SUPERUSER ALREADY EXISTS.')
 "
 
-echo "STEP SEVEN ::: NGINX CONFIGARATION"
+echo "STEP SEVEN ::: NGINX CONFIGURATION TEST"
 docker-compose -f docker-compose.prod.yml exec nginx nginx -t
 
 echo "STEP EIGHT ::: RELOAD NGINX"
-docker-compose -f docker-compose.prod.yml exec -s reload
+docker-compose -f docker-compose.prod.yml exec nginx nginx -s reload
 
-echo "PRODUCTION PROJECT RUN SUCCESSFULLY"
+echo "PRODUCTION PROJECT DEPLOYED SUCCESSFULLY"
