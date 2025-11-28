7c90ceb9db12a76b4968b1f179c6a46fed854b03
programmerrm <programmerwebrm@gmail.com>
2025-11-28 10:18:21 +0600

    new setup deploy.prod.sh

--- deploy.prod.sh
+++ deploy.prod.sh
@@ -19,22 +19,6 @@ docker-compose -f docker-compose.prod.yml exec backend python manage.py migrate 
 echo "STEP FIVE ::: COLLECT STATIC FILES"
 docker-compose -f docker-compose.prod.yml exec backend python manage.py collectstatic --noinput
 
-echo "STEP SIX ::: CREATE SUPERUSER"
-docker-compose -f docker-compose.prod.yml exec -T backend python << EOF
-import os
-from django.contrib.auth import get_user_model
-User = get_user_model()
-email = os.getenv('DJANGO_SUPERUSER_EMAIL', 'info@eic.com.bd')
-username = os.getenv('SUPERUSER_USERNAME', 'eicadmin')
-password = os.getenv('SUPERUSER_PASSWORD', 'strongpassword123')
-
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(email=email, username=username, password=password)
-    print('SUPERUSER CREATED SUCCESSFULLY!')
-else:
-    print('SUPERUSER ALREADY EXISTS.')
-EOF
-
 echo "STEP SEVEN ::: NGINX CONFIGARATION"
 docker-compose -f docker-compose.prod.yml exec nginx nginx -t
 
