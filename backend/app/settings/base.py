0c0ecd8f8e0775e134b88ee39fafd711e4dc5055
programmerrm <programmerwebrm@gmail.com>
2025-11-28 13:21:04 +0600

    new setup deploy.prod.sh

--- backend/app/settings/base.py
+++ backend/app/settings/base.py
@@ -104,9 +104,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "assets")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # collectstatic target
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "assets")]  # optional extra static
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
