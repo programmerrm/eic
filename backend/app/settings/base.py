981a06e7a62ce9eb24fe55a6364f8b40cffd6697
programmerrm <programmerwebrm@gmail.com>
2025-11-28 13:09:25 +0600

    new setup deploy.prod.sh

--- backend/app/settings/base.py
+++ backend/app/settings/base.py
@@ -107,11 +107,11 @@ TIME_ZONE = 'Asia/Dhaka'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
