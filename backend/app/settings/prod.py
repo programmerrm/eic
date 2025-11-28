397f043e58b14eff37a0c901c6b95eccd8c8abe0
programmerrm <programmerwebrm@gmail.com>
2025-11-28 17:30:01 +0600

    header footer banner section added

--- backend/app/settings/prod.py
+++ backend/app/settings/prod.py
@@ -7,7 +7,12 @@ from app.settings.base import *
 
 # BASE CONFIGRATION
 DEBUG = False
-ALLOWED_HOSTS = ['eic.com.bd', 'www.eic.com.bd', 'eicsec.com', 'www.eicsec.com']
+ALLOWED_HOSTS = [
+    'eic.com.bd',
+    'www.eic.com.bd',
+    'eicsec.com',
+    'www.eicsec.com',
+]
 SECRET_KEY = 'django-insecure--j7#f^zo#_&rlptdsq0ut#8)cdnocjkb=$7*=(vyc4z%5zizum'
 
 # DATABASE CONFIGRATION
@@ -30,3 +35,10 @@ CSRF_TRUSTED_ORIGINS = [
     'https://www.eicsec.com',
 ]
 
+# CORS ALLOWED ORIGINS
+CORS_ALLOWED_ORIGINS = [
+    "https://eic.com.bd",
+    "https://www.eic.com.bd",
+    "https://eicsec.com",
+    "https://www.eicsec.com",
+]
