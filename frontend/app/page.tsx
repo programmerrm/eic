ff33197f22c8c147f1a813c7ff38573ec256ad69
programmerrm <programmerwebrm@gmail.com>
2025-11-29 17:48:18 +0600

    home page done

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,9 +5,11 @@ import Cybersecurity from "@/components/cybersecurity/cybersecurity";
 import Features from "@/components/features/features";
 import GloballyAccredited from "@/components/globally-accredited/GloballyAccredited";
 import OurProvenProcessSecurity from "@/components/our-proven-process-security/our-proven-process-security";
+import Review from "@/components/review/review";
 import SecurityFirm from "@/components/security-firm/security-firm";
 import HomePageRelated from "@/components/services/homepageRelated";
 import HomePageSuccessStoriesRelated from "@/components/success-stories/HomePageSuccessStoriesRelated";
+import WhyChoose from "@/components/why-choose/why-choose";
 
 export default function Page() {
     return (
@@ -18,6 +20,8 @@ export default function Page() {
             <Cybersecurity />
             <HomePageRelated />
             <OurProvenProcessSecurity />
+            <WhyChoose />
+            <Review />
             <HomePageSuccessStoriesRelated />
             <GloballyAccredited />
             <HomePageRelatedBlog />
--- frontend/components/why-choose/why-choose.tsx
+++ frontend/components/why-choose/why-choose.tsx
@@ -4,7 +4,9 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default async function WhyChoose() {
-
+    const experienceEic = await getFetchData('/homepage/experience-eic/', {
+        tag: 'experience-eic-data',
+    })
     const experienceEicItem = await getFetchData('/homepage/experience-eic-item/', {
         tag: 'experience-eic-item-data',
     });
@@ -15,7 +17,7 @@ export default async function WhyChoose() {
                 <div className="flex flex-col lg:flex-row justify-between">
                     <div className="w-full lg:w-1/2 order-2 lg:order-1">
                         <div className="w-full lg:w-[450px] max-w-[370px] lg:max-w-[450px] pt-10 lg:pt-74">
-                            <h2>Experience the <span className="text-blue">EIC Impact</span></h2>
+                            <h2>{experienceEic?.data?.normal_title} <span className="text-blue">{experienceEic?.data?.span_title}</span></h2>
                             {experienceEicItem?.data && (
                                 <ul className="mt-8 w-full max-w-[380px] xl:max-w-[450px]">
                                     {experienceEicItem?.data?.map((item: any) => {
@@ -25,7 +27,7 @@ export default async function WhyChoose() {
                                     })}
                                 </ul>
                             )}
-                            <Link href="" className="btn-primary group inline-flex mt-10">Secure Your DATA
+                            <Link href={experienceEic?.data?.btn_url} className="btn-primary group inline-flex mt-10">{experienceEic?.data?.btn_name}
                                 <svg
                                     className="transition-all duration-500 group-hover:rotate-45 w-5 md:w-6 h-5 md:h-6"
                                     xmlns="http://www.w3.org/2000/svg"
@@ -46,12 +48,14 @@ export default async function WhyChoose() {
                     <div className="w-full lg:w-1/2 flex items-center justify-center lg:justify-end order-1 lg:order-2">
                         <div
                             className="pci-wrap w-full max-w-[250px] sm:max-w-[350px] md:max-w-[420px] lg:max-w-[687px] max-h-[740px] pt-20 sm:pt-33 lg:pt-48 xl:pt-56 mb-20 sm:mb-28 md:mb-20 xl:mb-60 relative flex items-center justify-center lg:justify-end">
-                            <Image
-                                src="./src/assets/image/eic-impact.svg"
-                                alt="eic"
-                                width={687}
-                                height={742}
-                            />
+                            {experienceEic?.data?.image && (
+                                <Image
+                                    src={experienceEic?.data?.image}
+                                    alt="eic"
+                                    width={687}
+                                    height={742}
+                                />
+                            )}
                         </div>
                     </div>
                 </div>
