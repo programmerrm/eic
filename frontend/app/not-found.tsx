8a44d48b9a86c57ccc9f6598f016c9ad213ebd5d
programmerrm <programmerwebrm@gmail.com>
2025-11-30 21:23:08 +0600

    fix

--- frontend/app/not-found.tsx
+++ frontend/app/not-found.tsx
@@ -1,26 +1,24 @@
 import Image from "next/image";
 import NotFoundImg from "@/public/images/not-found.svg";
-import { getFetchData } from "@/utils/getFetchData";
-import { MEDIA_URL } from "@/utils/api";
 
-export default async function NotFound() {
-    const notFoundData = await getFetchData('/configuration/not-found-content/', {
-        tag: 'not-found-content-data',
-    });
+export default function NotFound() {
     return (
         <section className="error-section">
             <div className="container">
                 <div className="error-image">
                     <Image
-                        src={`${MEDIA_URL}${notFoundData?.data?.image}` || NotFoundImg}
+                        src={NotFoundImg}
                         alt="not-found-icon"
                         width={302}
                         height={300}
                     />
                 </div>
                 <div className="error-content">
-                    <h4>{notFoundData?.data?.title || "Oops! This page has gone missing."}</h4>
-                    <p>{notFoundData?.data?.description || "Uh-oh! This page does exist. It might have moved or been deleted. Head to our homepage or explore below!"}</p>
+                    <h4>Oops! This page has gone missing.</h4>
+                    <p>
+                        Uh-oh! This page does exist. It might have moved or been
+                        deleted. Head to our homepage or explore below!
+                    </p>
                 </div>
             </div>
         </section>
