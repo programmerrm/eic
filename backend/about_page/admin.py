7722557d4cf86a12279b0353873141b81fced7f4
programmerrm <programmerwebrm@gmail.com>
2026-02-02 15:14:55 +0600

    about page model fix

--- backend/about_page/admin.py
+++ backend/about_page/admin.py
@@ -1,23 +1,5 @@
 from django.contrib import admin
-from about_page.models import AboutTopBar, SecureFutureTopBar, SecureFutureItem, SecurityFirm, DigitalSecuritySolutionTopBar, DigitalSecuritySolutionItem, HappyJourneyTopBar, HappyJourneyItem,SeoTag, AboutPageSchema, Organization
-
-class OrganizationInline(admin.StackedInline):
-    model = Organization
-    extra = 0
-    max_num = 1
-
-@admin.register(AboutPageSchema)
-class AboutPageSchemaAdmin(admin.ModelAdmin):
-    inlines = [OrganizationInline]
-
-    list_display = ("name", "url")
-    fieldsets = (
-        ("Aboutpage Info", {
-            "fields": ("name", "url", "description")
-        }),
-    )
-    
-admin.site.register(SeoTag)
+from about_page.models import AboutTopBar, SecureFutureTopBar, SecureFutureItem, SecurityFirm, DigitalSecuritySolutionTopBar, DigitalSecuritySolutionItem, HappyJourneyTopBar, HappyJourneyItem
 
 # Register your models here.
 admin.site.register(AboutTopBar)
